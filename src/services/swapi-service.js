e2d68615982cc845ce850cc4b5fdcebb18e136da
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-06 21:05:48 +0300

    move service to folder

---
+++ src/services/swapi-service.js
@@ -0,0 +1,42 @@
+export default class SwapiService {
+
+  _apiBase = 'https://swapi.dev/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`)
+    }
+    return await res.json();
+  };
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    console.log(res)
+    return res.results;
+  };
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  };
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    console.log(res)
+    return res.results;
+  };
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  };
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    console.log(res)
+    return res.results;
+  };
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}`);
+  };
+};
