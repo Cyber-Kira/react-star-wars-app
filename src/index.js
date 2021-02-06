01190bf8c0040dbd982f5fd4faa1f52197ce35d8
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-06 20:57:05 +0300

    create apiClient

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('Hello World');
+class SwapiService {
+
+  _apiBase = 'https://swapi.dev/api';
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, received ${res.status}`)
+    }
+    return await res.json();
+  };
+
+  async getAllPeople() {
+    const res = await this.getResource(`/people/`);
+    console.log(res)
+    return res.results;
+  };
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  };
+
+  async getAllPlanets() {
+    const res = await this.getResource(`/planets/`);
+    console.log(res)
+    return res.results;
+  };
+
+  getPlanet(id) {
+    return this.getResource(`/planets/${id}`);
+  };
+
+  async getAllStarships() {
+    const res = await this.getResource(`/starships/`);
+    console.log(res)
+    return res.results;
+  };
+
+  getStarship(id) {
+    return this.getResource(`/starships/${id}`);
+  };
+};
+
+const swapi = new SwapiService();
+
+swapi.getPerson(3).then((p) => console.log(p.name))
