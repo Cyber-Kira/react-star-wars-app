2cd9ba0d8294afa583b443fdd382fac37e133a16
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-07 23:06:49 +0300

    add transformer functions

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -27,14 +27,8 @@ export default class RandomPlanet extends Component {
     this.swapiService
       .getPlanet(id)
       .then((planet) => {
-        this.setState({
-          id,
-          name: planet.name,
-          population: planet.population,
-          rotationPeriod: planet.rotation_period,
-          diameter: planet.diameter
-        });
-      })
+        this.setState();
+      });
   };
 
   render() {
