786c1323b78d8e3fb9e5263e39d2b548865f8470
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-07 22:45:02 +0300

    add planet data to state, create function for getting planet

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,27 +1,60 @@
 import React, { Component } from 'react';
 
+import SwapiService from '../../services/swapi-service';
+
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
+
+  swapiService = new SwapiService();
+
+  state = {
+    name: null,
+    population: null, 
+    rotationPeriod: null,
+    diameter: null
+  };
+
+  constructor() {
+    super();
+
+    this.updatePlanet();
+  }
+
+  updatePlanet() {
+    this.swapiService.getPlanet(7)
+      .then((planet) => {
+        this.setState({
+          name: planet.name,
+          population: planet.population,
+          rotationPeriod: planet.rotation_period,
+          diameter: planet.diameter
+        });
+      })
+  };
+
   render() {
+
+    const {name, population, rotationPeriod, diameter} = this.state;
+
     return (
       <div className="random-planet jumbotron rounded">
         <img className="planet-image"
            src={`https://starwars-visualguide.com/assets/img/planets/12.jpg`} alt="planet"/>
         <div>
-          <h4>Something</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
-              <span>Something</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Rotation Period</span>
-              <span>Something</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Diameter</span>
-              <span>Something</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
