7a2d76c36760d0d9f3e58262d935497422742c9b
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-08 22:01:52 +0300

    create page for people, using error boundry to prevent errors

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 import ErrorIndicator from '../error-indicator';
 
 import './app.css';
+import PeoplePage from '../people-page';
 
 export default class App extends Component {
 
@@ -22,12 +21,6 @@ export default class App extends Component {
         showRandomPlanet: !state.showRandomPlanet
       }
     });
-  }
-
-  onPersonSelected = (id) => {
-    this.setState({ 
-      selectedPerson: id
-    })
   }
 
   componentDidCatch() {
@@ -58,14 +51,7 @@ export default class App extends Component {
 
         { planet }
 
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onPersonSelected={ this.onPersonSelected } />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={ this.state.selectedPerson }/>
-          </div>
-        </div>
+        <PeoplePage />
       </div>
     );
   }
