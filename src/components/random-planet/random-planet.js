f3c45a7d82da4b2bcb54045aff6a010ce77259cf
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-08 17:47:56 +0300

    catching error, rendering error component

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import SwapiService from '../../services/swapi-service';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 import './random-planet.css';
 
@@ -10,7 +11,9 @@ export default class RandomPlanet extends Component {
   swapiService = new SwapiService();
 
   state = {
-    planet: []
+    planet: [],
+    loading: true,
+    error: false
   };
 
   constructor() {
@@ -20,22 +23,53 @@ export default class RandomPlanet extends Component {
   };
 
   onPlanetLoaded = (planet) => {
-    this.setState({ planet });
+    this.setState({ 
+      planet,
+      loading: false
+    });
+  };
+
+  onError = (error) => {
+    this.setState({ 
+      error: true,
+      loading: false
+    });
   };
 
   updatePlanet() {
     const id = Math.floor(Math.random() * 25 + 2);
     this.swapiService
       .getPlanet(id)
-      .then(this.onPlanetLoaded);
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
   };
 
   render() {
 
-    const { planet: { name, population, rotationPeriod, diameter, id } } = this.state;
+    const { planet, loading, error } = this.state;
+
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = hasData ? <PlanetView planet={planet} /> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
+        {spinner}
+        {content}
+      </div>
+    );
+  };
+};
+
+const PlanetView = ({ planet }) => {
+
+  const { name, population, rotationPeriod, diameter, id } = planet;
+
+  return (
+    <>
         <img className="planet-image"
            src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="planet"/>
         <div>
@@ -55,7 +89,6 @@ export default class RandomPlanet extends Component {
             </li>
           </ul>
         </div>
-      </div>
-    );
-  }
-}
+    </>
+  )
+};
