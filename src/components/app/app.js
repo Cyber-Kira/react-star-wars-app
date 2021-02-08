746edce04bff5c8c492c00231206149579071932
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-08 20:07:13 +0300

    rename props

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -51,7 +51,7 @@ export default class App extends Component {
             <ItemList onPersonSelected={ this.onPersonSelected } />
           </div>
           <div className="col-md-6">
-            <PersonDetails presonaId={ this.state.selectedPerson }/>
+            <PersonDetails personId={ this.state.selectedPerson }/>
           </div>
         </div>
       </div>
