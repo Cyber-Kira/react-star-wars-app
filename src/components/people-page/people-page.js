4c0418676fed07e169e21426ece273a93e3ef877
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-10 00:25:19 +0300

    import Row component

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -5,20 +5,7 @@ import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
 import SwapiService from '../../services/swapi-service';
-
-
-const Row = ({ left, right }) => {
-  return (
-    <div className="row mb2">
-      <div className="col-md-6">
-        {left}
-      </div>
-      <div className="col-md-6">
-      {right}
-      </div>
-    </div>
-  )
-}
+import Row from '../row';
 export default class PeoplePage extends Component {
 
   swapiService = new SwapiService();
