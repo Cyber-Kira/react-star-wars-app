1066ad8e31d2eeb4f8993f4ff51eccf7e799345b
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-10 00:21:15 +0300

    create wrapper component for rendering components from props

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -6,6 +6,19 @@ import PersonDetails from '../person-details';
 import ErrorIndicator from '../error-indicator/error-indicator';
 import SwapiService from '../../services/swapi-service';
 
+
+const Row = ({ left, right }) => {
+  return (
+    <div className="row mb2">
+      <div className="col-md-6">
+        {left}
+      </div>
+      <div className="col-md-6">
+      {right}
+      </div>
+    </div>
+  )
+}
 export default class PeoplePage extends Component {
 
   swapiService = new SwapiService();
@@ -33,18 +46,19 @@ export default class PeoplePage extends Component {
       return <ErrorIndicator />;
     }
 
-    return (
-      <div className="row mb2">
-        <div className="col-md-6">
+    const itemList = (
           <ItemList 
             onPersonSelected={ this.onPersonSelected }
             getData={ this.swapiService.getAllPeople }
             renderItem={({ name, gender, birthYear }) => `${name} (${gender}, ${birthYear})`} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={ this.state.selectedPerson }/>
-        </div>
-    </div>
+    );
+
+    const personDetails = (
+      <PersonDetails personId={ this.state.selectedPerson }/>
+    );
+
+    return (
+      <Row left={itemList} right={personDetails} />
     )
   }
 };
