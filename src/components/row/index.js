c019bc72d828ab69731ed3b8c359187723ef4038
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-12 22:42:37 +0300

    rename components

--- src/components/row/index.js
+++ src/components/row/index.js
@@ -1,16 +1,3 @@
-import React from 'react';
-
-const Row = ({ left, right }) => {
-  return (
-    <div className="row mb2">
-      <div className="col-md-6">
-        {left}
-      </div>
-      <div className="col-md-6">
-      {right}
-      </div>
-    </div>
-  )
-};
+import Row from './row';
 
 export default Row;
