600b886b6517ae29e2d9a9e5bdd3d0263418e16d
Cyber-Kira <kirillkoloskovm@gmail.com>
2021-02-13 15:32:11 +0300

    add records for each component

--- src/components/sw-components/details.js
+++ src/components/sw-components/details.js
@@ -1,6 +1,7 @@
 import { ItemDetails } from '../item-details';
 import { withDetails } from '../hoc-helpers/';
 import SwapiService from '../../services';
+import {Record} from '../item-details';
 
 const swapiService = new SwapiService();
 
@@ -13,11 +14,26 @@ const {
   getStarshipImage
 } = swapiService;
 
-const PersonDetails = withDetails(ItemDetails, getPerson, getPersonImage);
+const personRecord = [
+  <Record field="gender" label="Gender"/>,
+  <Record field="eyeColor" label="Eye color"/>
+];
 
-const PlanetDetails = withDetails(ItemDetails, getPlanet, getPlanetImage);
+const PlanetRecord = [
+  <Record field="population" label="Rotation Period"/>,
+  <Record field="diameter" label="Diameter"/>
+];
 
-const StarshipDetails = withDetails(ItemDetails, getStarship, getStarshipImage);
+const StarshipRecord = [
+  <Record field="model" label="Model"/>,
+  <Record field="costInCredits" label="Cost"/>
+];
+
+const PersonDetails = withDetails(ItemDetails, personRecord, getPerson, getPersonImage);
+
+const PlanetDetails = withDetails(ItemDetails, PlanetRecord, getPlanet, getPlanetImage);
+
+const StarshipDetails = withDetails(ItemDetails, StarshipRecord, getStarship, getStarshipImage);
 
 export {
   PersonDetails,
